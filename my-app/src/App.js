160810f0003780611f3b4f076f98946c35c49976
Knight <1051147003@qq.com>
2018-12-11 10:53:00 +0800

    create React App
    create React App

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const array = [4,2,6,5,7,1,9,3,8]
+const color = ['#333333', '#999999']
+
+const content = array.map((value, key) => {
+  const row = (
+      <font key={'item'+key} style={{color: color[key%2]}}>{value}&nbsp;&nbsp;&nbsp;</font>
+  )
+  console.log(value)
+  return row
+})
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {content}
+      </div>
+    );
+  }
+}
+
+export default App;
