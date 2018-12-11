53cabf5a862912e4ab995559f9a0d9bb3fa4681e
Knight <1051147003@qq.com>
2018-12-11 15:07:41 +0800

    增加开发配置
    添加Eslint方案
    添加babel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const array = [4,2,6,5,7,1,9,3,8]
-const color = ['#333333', '#999999']
+const array = [4, 2, 6, 5, 7, 1, 9, 3, 8];
+const color = ['#333333', '#999999'];
 
 const content = array.map((value, key) => {
   const row = (
-      <font key={'item'+key} style={{color: color[key%2]}}>{value}&nbsp;&nbsp;&nbsp;</font>
-  )
-  console.log(value)
-  return row
-})
+    <i key={`item${key}`} style={{ color: color[key % 2] }}>
+      {value}
+      &nbsp;&nbsp;&nbsp;
+    </i>
+  );
+  // console.log(value)
+  return row;
+});
 
 class App extends Component {
   render() {
